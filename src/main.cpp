8f699f2798b35a97ca280df94bafbde1f36455f4
LuckierBread <yoh.asuchra@hotmail.com>
2024-12-25 22:00:16 -0700

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,97 @@
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <FastLED.h>
+#include "credentials.h"
+
+#define NUM_LEDS 1
+#define DATA_PIN 48
+
+CRGB leds[NUM_LEDS];
+
+const char *ssid = MY_WIFI_SSID;
+const char *password = MY_WIFI_PASS;
+
+void setup()
+{
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(4);
+  Serial.begin(115200);
+  Serial.println("Booting");
+  leds[0] = CRGB::Yellow;
+  FastLED.show();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection Failed! Rebooting...");
+    leds[0] = CRGB::Red;
+    FastLED.show();
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname("myesp32");
+
+  // No authentication by default
+  ArduinoOTA.setPassword("MikesTestPass");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA
+      .onStart([]()
+               {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type); })
+      .onEnd([]()
+             { Serial.println("\nEnd"); })
+      .onProgress([](unsigned int progress, unsigned int total)
+                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+      .onError([](ota_error_t error)
+               {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      } });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  leds[0] = CRGB::Blue;
+  FastLED.show();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  ArduinoOTA.handle();
+  leds[0] = CRGB::Blue;
+  FastLED.show();
+  delay(1000);
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(1000);
+}
